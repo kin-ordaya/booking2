57c3c59b2479702cbc4e924f207792054441ee89
Aokifree20 <kinol787@gmail.com>
2025-09-08 13:00:33 -0500

    Fix
    Fix post DJ

--- src/aula/aula.controller.ts
+++ src/aula/aula.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { AulaService } from './aula.service';
 import { CreateAulaDto } from './dto/create-aula.dto';
 import { UpdateAulaDto } from './dto/update-aula.dto';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
@@ -15,35 +24,51 @@ export class AulaController {
 
   @Post()
   @Roles('ADMINISTRADOR')
-  @ApiOperation({ summary: 'Crear aula' , description: 'Crear aula' })
+  @ApiOperation({
+    summary: 'Crear aula',
+    description: 'Crea un nuevo registro de aula fisica en el sistema.\n\n**Roles permitidos:** ADMINISTRADOR',
+  })
   create(@Body() createAulaDto: CreateAulaDto) {
     return this.aulaService.create(createAulaDto);
   }
 
   @Get()
   @Roles('ADMINISTRADOR')
-  @ApiOperation({ summary: 'Obtener todos los aulas' , description: 'Obtener todos los aulas' })
+  @ApiOperation({
+    summary: 'Obtener todas las aulas',
+    description: 'Obtener todas las aulas fisicas del sistema.',
+  })
   findAll() {
     return this.aulaService.findAll();
   }
 
   @Get(':id')
   @Roles('ADMINISTRADOR')
-  @ApiOperation({ summary: 'Obtener una aula' , description: 'Obtener una aula' })
+  @ApiOperation({
+    summary: 'Obtener una aula',
+    description: 'Obtener una aula fisica del sistema por su ID.',
+  })
   findOne(@Param('id') id: string) {
     return this.aulaService.findOne(id);
   }
 
   @Patch(':id')
   @Roles('ADMINISTRADOR')
-  @ApiOperation({ summary: 'Actualizar una aula' , description: 'Actualizar una aula' })
+  @ApiOperation({
+    summary: 'Actualizar una aula',
+    description:
+      'Actualizar una aula fisica del sistema por su ID y datos de actualización.',
+  })
   update(@Param('id') id: string, @Body() updateAulaDto: UpdateAulaDto) {
     return this.aulaService.update(id, updateAulaDto);
   }
 
   @Delete(':id')
   @Roles('ADMINISTRADOR')
-  @ApiOperation({ summary: 'Eliminar una aula' , description: 'Eliminar una aula' })
+  @ApiOperation({
+    summary: 'Eliminar una aula',
+    description: 'Eliminar logicamente una aula fisica del sistema por su ID.',
+  })
   remove(@Param('id') id: string) {
     return this.aulaService.remove(id);
   }
--- src/declaracion_jurada/declaracion_jurada.service.ts
+++ src/declaracion_jurada/declaracion_jurada.service.ts
@@ -67,6 +67,7 @@ export class DeclaracionJuradaService {
       const declaracionJuradaExists =
         await this.declaracionJuradaRepository.findOne({
           where: {
+            estado: '1',
             rolUsuario: { id: rol_usuario_id },
             recurso: { id: recurso_id },
           },
