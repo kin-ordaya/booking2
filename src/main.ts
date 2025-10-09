ac7c1cc062e86851d1dca8a0de4cfc7812a2a926
Aokifree20 <kinol787@gmail.com>
2025-10-09 11:38:58 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,14 +25,14 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://bookingravts.continental.edu.pe',
-      'http://bookingravts.continental.edu.pe',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Authorization'],
+    // origin: [
+    //   'http://localhost:4200',
+    //   'https://bookingravts.continental.edu.pe',
+    //   'http://bookingravts.continental.edu.pe',
+    // ],
+    // credentials: true,
+    // methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    // allowHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
