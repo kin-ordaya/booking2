01f9f0bef5bea597f3533c3801614292d2ff8b0b
kinol <147191684+kin-ordaya@users.noreply.github.com>
2026-02-11 16:00:16 -0500

    Remove recurso_curso and recurso_curso_periodo
    Delete all files related to the recurso_curso and recurso_curso_periodo features: DTOs, entities, controllers, services, modules and test specs. This removes create/update/pagination DTOs, entity definitions, controllers, service implementations and related unit tests for both recurso-curso and recurso-curso-periodo components, cleaning up the codebase by removing these features.

--- src/recurso_curso/dto/update-recurso_curso.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRecursoCursoDto } from './create-recurso_curso.dto';
-
-export class UpdateRecursoCursoDto extends PartialType(CreateRecursoCursoDto) {}
--- src/recurso_curso_periodo/dto/update-recurso_curso_periodo.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRecursoCursoPeriodoDto } from './create-recurso_curso_periodo.dto';
-
-export class UpdateRecursoCursoPeriodoDto extends PartialType(
-  CreateRecursoCursoPeriodoDto,
-) {}
